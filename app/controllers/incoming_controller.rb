ade46919df1f444806ae8048bc80521778bad477
Wesley Ellis <tahnok@gmail.com>
2013-06-01 13:31:36 -0400

    move stuff

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,7 +1,20 @@
-class IncomingController < ApplicationController
+class IncommingController < ApplicationController
 
   def parse
     Rails.logger.info(params)
+
+    method, method_params = params["content"].split(" ", 2)
+    begin
+      object.send("call_#{method}".to_sym, params, method_params)
+    rescue
+
+    end
+
     render nothing: true, status: 200
   end
+
+  private
+    def call_hello(params, method_params)
+      {"messages" => {"content" => "Goodbye #{method_params}"}}.to_json
+    end
 end
