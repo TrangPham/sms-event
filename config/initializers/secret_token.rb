7ab1f316d806b053b0feb027a5281112d89cd4a8
Trang Pham <trang.pham@cenx.com>
2013-06-01 10:31:56 -0400

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmsEvent::Application.config.secret_token = 'fa133399712c1ffd68435456c07e1b69b014cb8e40218a4aa55ea8e2eba5d3f74d8626ffd43c648d5d39c6d04c8749477de7f9b0bfbb7d9b7df0d1383d9ffd1a'
