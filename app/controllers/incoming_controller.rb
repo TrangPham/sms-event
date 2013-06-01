719a870094c12c4f2355b46b7a3e1b7c84b165df
Wesley Ellis <tahnok@gmail.com>
2013-06-01 13:53:12 -0400

    actually working

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,9 +5,11 @@ class IncomingController < ApplicationController
 
     begin
       method, method_params = params["content"].split(" ", 2)
-      response = object.send("call_#{method}".to_sym, params, method_params)
+      binding.pry
+      response = send("call_#{method}".to_sym, params, method_params)
       render json: response
-    rescue
+    rescue StandardError
+      Rails.logger.info("invalid, try again")
       render nothing: true, status: 200
     end
   end
