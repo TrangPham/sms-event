b4df0f08bb87291e533ae04ea33f90104f19523f
Simeon Seguin <Simeon.se@gmail.com>
2013-06-01 21:14:03 -0400

    implemented call_settings method

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,13 +1,9 @@
 class IncomingController < ApplicationController
 
-<<<<<<< HEAD
-  VALID_SETTINGS = {
-  	""
-  }
-=======
+
   VALID_SETTINGS = ["talkback", "broadcast", "notify", "confirm"]
 
->>>>>>> 92827c02cfcf61fa5ea0d2ee5dd91f3e77bd59e0
+
   VALID_COMMANDS = {
     "register" => "register [event id]",
     "help" => "Available commands: register, create, unregister, message, status. Send 'help COMMAND' for more info",
@@ -35,9 +31,70 @@ class IncomingController < ApplicationController
   private
 
   def call_settings(params, method_params)
-      toggle, list = method_params.split(" ", 2)
-      if toggle == "toggle"
+  	event_id = method_params[0]
+  	event = Event.find_by_event_id(event_id)
+    return "Event #{event_id} does not exist" if event.nil? 
+
+    if event.organizer.phone == params["from_number"]
+      list = method_params.split(" ")
+      if list[0] == "toggle"
+      	 list.split(" ").each do |p| 
+      	   if VALID_SETTINGS.include?(p)
+      	     if p == VALID_SETTINGS[0]
+      	       event.talkback = !event.talkback
+      	     elsif p == VALID_SETTINGS[1]
+      	   	   event.broadcast = !event.broadcast
+      	     elsif p == VALID_SETTINGS[2]
+      	       event.notify = !event.notify
+      	     elsif p == VALID_SETTINGS[3]
+        	   event.confirm = !event.confirm
+      	     else
+      	       return "Invalid setting:  #{p} "
+      	     end
+           end
+         end
+      elsif list[0] == "on"
       	list.split(" ").each do |p| 
+      	   if VALID_SETTINGS.include?(p)
+      	     if p == VALID_SETTINGS[0]
+      	       event.talkback = true
+      	     elsif p == VALID_SETTINGS[1]
+      	   	   event.broadcast = true
+      	     elsif p == VALID_SETTINGS[2]
+      	       event.notify = true
+      	     elsif p == VALID_SETTINGS[3]
+        	   event.confirm = true
+      	     else
+      	       return "Invalid setting:  #{p} "
+      	     end
+           end
+
+        end
+       else list[0] == "off"
+       	list.split(" ").each do |p| 
+      	   if VALID_SETTINGS.include?(p)
+      	     if p == VALID_SETTINGS[0]
+      	       event.talkback = false
+      	     elsif p == VALID_SETTINGS[1]
+      	   	   event.broadcast = false
+      	     elsif p == VALID_SETTINGS[2]
+      	       event.notify = false
+      	     elsif p == VALID_SETTINGS[3]
+        	   event.confirm = false
+      	     else
+      	       return "Invalid setting:  #{p} "
+      	     end
+           end
+         end
+       end
+    else
+    	return "Invalid authorization"
+    end
+
+  end
+  
+
+
 
           
 
