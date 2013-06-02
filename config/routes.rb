5709c0689de239ff189224d223178278312361ae
Trang Pham <trang.pham@cenx.com>
2013-06-02 13:23:09 -0400

    events route lists all events

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,8 @@ SmsEvent::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-
+  match 'events' => 'events#all'
   match 'incoming' => 'incoming#parse'
   match 'events/:id' => 'events#view'
+
 end
